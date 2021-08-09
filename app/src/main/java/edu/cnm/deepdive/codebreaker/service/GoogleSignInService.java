4f7d3ac0296f83e3cafc4259b31e2cadd2c7ceda
Jonathan Rodrigues <jonrodrigues24@gmail.com>
2021-08-09 12:09:20 -0600

    lunch 8/9

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/GoogleSignInService.java
@@ -11,16 +11,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import edu.cnm.deepdive.codebreaker.BuildConfig;
+import edu.cnm.deepdive.codebreaker.model.entity.User;
 import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
 
 public class GoogleSignInService {
-
 
   private static final String BEARER_TOKEN_FORMAT = "Bearer %s";
 
   private static Application context;
 
   private final GoogleSignInClient client;
+  private final UserRepository userRepository;
 
   private GoogleSignInAccount account;
 
@@ -32,6 +34,7 @@ public class GoogleSignInService {
         .requestIdToken(BuildConfig.CLIENT_ID)
         .build();
     client = GoogleSignIn.getClient(context, options);
+    userRepository = new UserRepository(context);
   }
 
   public static void setContext(Application context) {
@@ -48,22 +51,29 @@ public class GoogleSignInService {
 
   private void setAccount(GoogleSignInAccount account) {
     this.account = account;
-    //TODO remove following line after we start sending authenticated requests to service.
+    // TODO Remove following line after we start sending authenticated requests to service.
     Log.d(getClass().getName(), (account != null) ? account.getIdToken() : "(none)");
   }
 
   public Single<GoogleSignInAccount> refresh() {
     return Single.create((emitter) ->
         client.silentSignIn()
-          .addOnSuccessListener(this::setAccount)
-          .addOnSuccessListener(emitter::onSuccess)
-          .addOnFailureListener(emitter::onError)
+            .addOnSuccessListener(this::setAccount)
+            .addOnSuccessListener(emitter::onSuccess)
+            .addOnFailureListener(emitter::onError)
     );
   }
 
   public Single<String> refreshBearerToken() {
     return refresh()
         .map((account) -> String.format(BEARER_TOKEN_FORMAT, account.getIdToken()));
+  }
+
+  public Single<User> refreshUser() {
+    return refresh()
+        .observeOn(Schedulers.io())
+        .flatMap((account) ->
+            userRepository.getOrCreate(account.getId(), account.getDisplayName()));
   }
 
   public void startSignIn(Activity activity, int requestCode) {
@@ -84,14 +94,14 @@ public class GoogleSignInService {
   }
 
   public Task<Void> signOut() {
-
     return client.signOut()
         .addOnCompleteListener((ignored) -> setAccount(null));
-
   }
-  private static class InstanceHolder{
+
+  private static class InstanceHolder {
 
     private static final GoogleSignInService INSTANCE = new GoogleSignInService();
+
   }
 
 }
